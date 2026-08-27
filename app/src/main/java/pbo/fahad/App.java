2057956739e15e95ab467f7502b40635816e9251
Fahad-WSC <r3dalpha91@gmail.com>
2026-08-27 14:17:06 +0700

    tugas 2

--- app/src/main/java/pbo/fahad/App.java
+++ app/src/main/java/pbo/fahad/App.java
@@ -1,129 +1,240 @@
 package pbo.fahad;
 
-import java.util.Scanner;
+abstract class Hewan {
+    private String name;
+    private int age;
+    private String food;
 
-class Menu {
-    String name;
-    double price;
-    String category;
-    static int totalMenu;
-
-    public Menu(String name, double price, String category) {
+    public Hewan(String name, int age, String food) {
         this.name = name;
-        this.price = price;
-        this.category = category;
-        totalMenu++;
+        this.age = age;
+        this.food = food;
     }
 
-    public void showMenu() {
-        System.out.println("Name: " + name);
-        System.out.println("Price: Rp" + (int)price);
-        System.out.println("Category : " + category);
-        System.out.println();
+    public String getName() {
+        return name;
     }
 
-    public double discount(double percentage) {
-        double priceDiscount = price - (price * percentage / 100);
-        System.out.println("Harga setelah diskon: Rp" + (int)priceDiscount);
-        return priceDiscount;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public double changePrice(double newPrice) {
-        this.price = newPrice;
-        System.out.println("Harga menu telah diubah menjadi: Rp" + (int)price);
-        return price;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFood() {
+        return food;
+    } 
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public abstract String printSound();
+
+    public abstract void move();
+
+    public String printData() {
+        return "Name: " + "|" + name + "|" +"Age: " + "|" + age + "|" + "Food: " + food;
+    }
+}
+
+class HewanDarat extends Hewan {
+    int numberOfLegs;
+
+    public HewanDarat(String name, int age, String food, int numberOfLegs) {
+        super(name, age, food);
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    public void move() {
+        System.out.println("Aku hewan darat yang berjalan di darat");
+    }
+
+    @Override
+    public String printSound() {
+        return "ckckckck";
+    }
+
+    @Override
+    public String printData() {
+        return super.printData() + "Number of Legs: " + numberOfLegs;
     }
 }
 
-class Order {
-    String orderId;
-    String customerName;
-    Menu menu;
-    int quantity;
-
-    public Order(String orderId, String customerName, Menu menu, int quantity) {
-        this.orderId = orderId;
-        this.customerName = customerName;
-        this.menu = menu;
-        this.quantity = quantity;
+class Anjing extends HewanDarat {
+    public Anjing(String name, int age, String food, int numberOfLegs) {
+        super(name, age, food, numberOfLegs);
     }
 
-    public double calculateTotal() {
-        return menu.price * quantity;
-    }
-
-    public void showOrder() {
-        System.out.println("ID Pesanan: " + orderId);
-        System.out.println("Nama Pelanggan: " + customerName);
-        System.out.println("Menu Dipesan: " + menu.name);
-        System.out.println("Jumlah:" + quantity);
-        System.out.println("Total Bayar:" + (int)calculateTotal());
-
+    @Override
+    public String printSound() {
+        return "Guk guk";
     }
 }
+
+class Kucing extends HewanDarat {
+    public Kucing(String name, int age, String food, int numberOfLegs) {
+        super(name, age, food, numberOfLegs);
+    }
+
+    @Override
+    public String printSound() {
+        return "Meow meow";
+    }
+}
+
+class Tikus extends HewanDarat {
+    public Tikus(String name, int age, String food, int numberOfLegs) {
+        super(name, age, food, numberOfLegs);
+    }
+
+    @Override
+    public String printSound() {
+        return "Cit cit";
+    }
+}
+
+class HewanLaut extends Hewan {
+    int depth;
+
+    public HewanLaut(String name, int age, String food, int depth) {
+        super(name, age, food);
+        this.depth = depth;
+    }
+
+    public void move() {
+        System.out.println("Aku hewan laut");
+    }
+
+    @Override
+    public String printSound() {
+        return "splash splash";
+    }
+
+    @Override 
+    public String printData() {
+        return super.printData() + "Depth: " + depth;
+    }
+}
+
+class Ikan extends HewanLaut {
+    public Ikan(String name, int age, String food, int depth) {
+        super(name, age, food, depth);
+    }
+
+    @Override
+    public String printSound() {
+        return "Blub blub";
+    }
+}
+
+class Hiu extends HewanLaut {
+    public Hiu(String name, int age, String food, int depth) {
+        super(name, age, food, depth);
+    }
+
+    @Override
+    public String printSound() {
+        return "Splash";
+    }
+}
+
+class Paus extends HewanLaut {
+    public Paus(String name, int age, String food, int depth) {
+        super(name, age, food, depth);
+    }
+
+    @Override
+    public String printSound() {
+        return "Whistle";
+    }
+}
+
+class hewanUdara extends Hewan {
+    Double wingSpan;
+
+    public hewanUdara(String name, int age, String food, Double wingSpan) {
+        super(name, age, food);
+        this.wingSpan = wingSpan;
+    }
+
+    public void move() {
+        System.out.println("Aku hewan udara");
+    }
+
+    @Override
+    public String printSound() {
+        return "cirp cirp";
+    }
+
+    @Override
+    public String printData() {
+        return super.printData() + "Wing Span: " + wingSpan;
+    }
+}
+
+class Burung extends hewanUdara {
+    public Burung(String name, int age, String food, Double wingSpan) {
+        super(name, age, food, wingSpan);
+    }
+
+    @Override
+    public String printSound() {
+        return "Cuit cuit";
+    }
+}   
+
+class Elang extends hewanUdara {
+    public Elang(String name, int age, String food, Double wingSpan) {
+        super(name, age, food, wingSpan);
+    }
+
+    @Override
+    public String printSound() {
+        return "Screech";
+    }
+}
+
+class Bebek extends hewanUdara {
+    public Bebek(String name, int age, String food, Double wingSpan) {
+        super(name, age, food, wingSpan);
+    }
+
+    @Override
+    public String printSound() {
+        return "Kwek Kwek";
+    }
+}
+
+
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Menu menu1 = new Menu("Espresso", 20000, "Beverage");
-        Menu menu2 = new Menu("Croissant", 25000, "Food");
-        Menu menu3 = new Menu("Matcha", 22000, "Beverage");
-        Menu menu4 = new Menu("Sandwich", 30000, "Food");
+       Hewan[] daftarHewan = new Hewan[] {
+        new Anjing("Doggy", 3, "Tulang", 4),
+        new Kucing("Milo", 2, "Ikan", 4),
+        new Tikus("Jerry", 1, "Keju", 4),   
+        new Ikan("Nemo", 1, "Plankton", 15),
+        new Hiu("Megalo", 10, "Daging", 500),
+        new Paus("Bluey", 20, "Krill", 1000),
+        new Burung("Pipit"  , 1, "Biji-bijian", 0.2),
+        new Elang("Garuda", 5, "Daging", 2.1),
+        new Bebek("Donald", 2, "Dedak", 0.8)
+       };
 
-        Order orderKey = new Order("ORD-001", "Key", menu1, 2);
+       
+       for (Hewan h : daftarHewan) {
+        h.move();
+        System.out.println(h.printData());
+        System.out.println("Suara: " + h.printSound());
+        System.out.println("-----------------------------");
+       };
 
-        boolean isRunning = true;
-
-        while (isRunning) {
-            System.out.println("1. Tampilkan semua item menu");
-            System.out.println("2. Menampilkan Harga diskon pada item menu");
-            System.out.println("3. Mengubah harga item menu");
-            System.out.println("4. Menampilkan semua informasi item menu terbaru");
-            System.out.println("5. Tampilkan jumlah menu yang terdaftar");
-            System.out.println("6. Keluar");
-            System.out.println("7. Tampilkan informasi pesanan");
-            
-            System.out.print("Masukkan pilihan Anda (1-7): ");
-            int choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    menu1.showMenu();
-                    menu2.showMenu();
-                    menu3.showMenu();
-                    menu4.showMenu();
-                    break;
-                case 2:
-                    System.out.print(menu1.discount(10));
-                    System.out.print(menu2.discount(10));
-                    System.out.print(menu3.discount(10));
-                    System.out.print(menu4.discount(10));
-                    break;
-                case 3:
-                    System.out.print(menu1.changePrice(25000));
-                    System.out.print(menu2.changePrice(28000));
-                    System.out.print(menu3.changePrice(25000));
-                    System.out.print(menu4.changePrice(35000));
-                    break;
-                case 4:
-                    menu1.showMenu();
-                    menu2.showMenu();
-                    menu3.showMenu();
-                    menu4.showMenu();
-                    break;
-                case 5:
-                    System.out.println("Jumlah menu yang terdaftar: " + Menu.totalMenu);
-                    break;
-                case 6:
-                    System.out.println("Program selesai");
-                    isRunning = false;
-                    break;
-                case 7:
-                    orderKey.showOrder();
-                    break;
-                default:
-                    System.out.println("Pilihan tidak valid");
-            }
-        }
     }
 }
